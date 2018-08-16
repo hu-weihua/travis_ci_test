73a7452f1b3d6e56844483b4e3edb5eb18e51c86
Weihua Hu <huweihua@cn.ibm.com>
2018-08-16 17:25:57 +0800

    Update test_trigger.sh

--- test_trigger.sh
+++ test_trigger.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo "============================"
+export
 echo "this is $0"
 echo "buildid = $TRAVIS_BUILD_ID"
-echo "test = $TEST"
-
-export
-
+echo "TEST_KEY = $TEST_KEY"
+echo "TEST_TXT = $TEST_TXT"
+ping -c 2 $TEST_KEY 
 exit 0
